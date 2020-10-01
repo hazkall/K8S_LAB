669df9371aac2ede16511559174352252f75c727
lipenodias <64644915+lipenodias@users.noreply.github.com>
2020-10-01 14:18:22 -0300

    Add files via upload

---
+++ k8s.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#System preparation for K8S
+
+yum clean all
+yum update -y
+yum -y groupinstall 'Development Tools'
+yum -y install wget 
+curl -fsSL https://get.docker.com | bash
+systemctl enable docker
+systemctl start docker
+cat << EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo setenforce 0
+sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+sudo swapoff -a
+sed -i 's/.*swap.*/#&/' /etc/fstab
+systemctl stop firewalld
+systemctl disable firewalld
+sudo yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet && systemctl start kubelet
+cat << EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+systemctl daemon-reload
+systemctl restart kubelet
+
+#K8S Cluster Configuration
+kubeadm init --apiserver-advertise-address $(hostname -i)
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+#Network Pod Weaver
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
