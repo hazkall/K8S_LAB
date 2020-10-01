143d0774f859fbb9aafd249d7112587175e30f27
Felipe Nobrega <64644915+lipenodias@users.noreply.github.com>
2020-10-01 20:18:35 -0300

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -41,4 +41,5 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 #Network Pod Weaver
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl taint nodes --all node-role.kubernetes.io/master-
 
